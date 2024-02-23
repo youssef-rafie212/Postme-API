686ce8308962c0ff798ed216f20e3679759b01ae
youssef <joe69696974@gmail.com>
2024-02-23 13:51:45 +0200

    Fixed error middleware bug

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,13 @@
-import { Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from "express";
 import AppError from "utils/errors/app.error";
 
-const errorMiddleware = (err: AppError, req: Request, res: Response) => {
+const errorMiddleware = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const message = err.message || "Something went wrong";
   const status = err.status || 500;
 
